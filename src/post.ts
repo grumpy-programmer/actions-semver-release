0ce57a79226b7ad5e101ee6afd1d417eba5f5bec
Grumpy Programmer <very.grumpy.programmer@gmail.com>
2020-11-21 19:15:14 +0100

    feat: add release and version output

---
+++ src/post.ts
@@ -0,0 +1,36 @@
+import * as core from '@actions/core';
+import { GithubService } from './github';
+
+const github = new GithubService();
+
+async function main() {
+  const version = getState('version');
+  const released = getParsedState<boolean>('released');
+  const messages = getParsedState<string[]>('messages');
+
+  if (released) {
+    core.info(`Release: ${version}`);
+
+    await createRelease(version, messages);
+  }
+}
+
+async function createRelease(tag: string, messages: string[]) {
+  const changelog = messages.map(m => `* ${m}\n`).join('');
+  const body = `**Changelog:**\n${changelog}`;
+
+  await github.createRelease(tag, body);
+}
+
+function getParsedState<T>(key: string): T {
+  const state = getState(key);
+
+  return JSON.parse(state);
+}
+
+function getState(key: string): string {
+  return core.getState(key);
+}
+
+main()
+  .catch(e => core.error(e));
